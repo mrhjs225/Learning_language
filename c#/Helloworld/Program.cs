e980a9386046bae5da14f4412e7a8d5146355115
mrhjs225 <mrhjs@gmail.com>
2019-03-17 15:14:03 +0900

    c# setting

---
+++ c#/Helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helloworld // same as package
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
